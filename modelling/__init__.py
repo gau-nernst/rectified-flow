778d066f80855b32d06e9a1e2dbc2331bbedc9dc
Thien Tran <gau.nernst@yahoo.com.sg>
2024-12-03 15:05:46 +0800

    add face detection and embedder module

--- modelling/__init__.py
+++ modelling/__init__.py
@@ -1,6 +1,7 @@
 from .autoencoder import AutoEncoder, AutoEncoderConfig, load_autoencoder, load_flux_autoencoder
+from .face_embedder import FaceEmbedder, YOLOv8Face
 from .flux import Flux, FluxConfig, load_flux, load_flux_redux, load_shuttle
+from .iresnet import load_adaface_ir101
 from .lora import LoRALinear
 from .text_embedder import TextEmbedder, load_clip_text, load_t5
 from .utils import load_hf_state_dict
-from .iresnet import load_adaface_ir101
