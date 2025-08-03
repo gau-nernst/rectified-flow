ab59a3bb34bd70cc3e57093584abe65e997eadfd
Thien Tran <gau.nernst@yahoo.com.sg>
2025-08-03 19:16:15 +0800

    delete face stuff

--- modelling/__init__.py
+++ modelling/__init__.py
@@ -6,9 +6,7 @@ from .autoencoder import (
     load_sd3_autoencoder,
     load_sdxl_autoencoder,
 )
-from .face_embedder import FaceEmbedder, YOLOv8Face
 from .flux import Flux, FluxConfig, load_flux, load_flux_redux
-from .iresnet import IResNet, load_adaface_ir101
 from .lora import LoRALinear
 from .sd3 import SD3, load_sd3_5
 from .text_embedder import TextEmbedder, load_clip_l, load_openclip_bigg, load_t5
