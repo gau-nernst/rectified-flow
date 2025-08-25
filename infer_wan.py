fix timestep scaling for Wan

infer_wan.py
@@ -108,11 +108,11 @@ class Wan5BGenerator:
 
 # https://github.com/Wan-Video/Wan2.2/blob/031a9be5/wan/utils/fm_solvers_unipc.py
 # not sure why they use discrete timesteps
-# NOTE: we keep the last t=0
+# NOTE: we keep continuous timestep (but discretized to 1/1000) and keep the last t=0
 def wan_timesteps(num_steps: int = 50, shift: float = 5.0, num_train_steps: int = 1000):
     sigmas = torch.linspace(1 - 1 / num_train_steps, 0, num_steps + 1)
     sigmas = shift / (shift + 1 / sigmas - 1)
-    return (sigmas * num_train_steps).to(torch.int64).tolist()
+    return sigmas.mul(num_train_steps).round().div(num_train_steps).tolist()
 
 
 @torch.no_grad()
@@ -130,7 +130,8 @@ def wan_generate(
     solver_ = get_solver(solver)
 
     for i in tqdm(range(num_steps), disable=not pbar, dynamic_ncols=True):
-        t = torch.tensor([timesteps[i]], device="cuda")
+        # TODO: fold 1000 into modelling code
+        t = torch.tensor([timesteps[i] * 1000], device="cuda")
         v = wan(latents, t, context)
 
         # classifier-free guidance