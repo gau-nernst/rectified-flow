c0076c70d1ac2e1e9c3b68fb6e74ca8f32aaf4f7
Thien Tran <gau.nernst@yahoo.com.sg>
2026-01-17 20:22:44 +0800

    reorder

--- modelling/z_image/model.py
+++ modelling/z_image/model.py
@@ -81,13 +81,13 @@ class Block(nn.Module):
 
 @dataclasses.dataclass
 class ZImageConfig:
-    in_channels: int = 16
+    img_dim: int = 16
     txt_dim: int = 2560
+    mod_dim: int = 256
     dim: int = 3840
     n_refiner_layers: int = 2
     n_layers: int = 30
     patch_size: int = 2
-    mod_dim: int = 256
     mlp_ratio: float = 8 / 3
     rope_dims: tuple[int, int, int] = (32, 48, 48)
 
@@ -105,7 +105,7 @@ class ZImage(nn.Module):
         self.cap_pad_token = nn.Parameter(torch.zeros(1, cfg.dim))
 
         # image-only processing
-        patchified_dim = cfg.patch_size * cfg.patch_size * cfg.in_channels
+        patchified_dim = cfg.patch_size * cfg.patch_size * cfg.img_dim
         self.all_x_embedder = nn.ModuleDict()
         self.all_x_embedder["2-1"] = Linear(patchified_dim, cfg.dim)
         self.noise_refiner = nn.ModuleList(
